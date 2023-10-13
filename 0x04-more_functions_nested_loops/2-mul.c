12992fade613d30f7bc2f6066b77deafce6aba4e
Aboubaker fanti <aboubakerfanti1@gmail.com>
2023-10-13 14:26:37 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - input
+ * Description: multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a * b 
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
